5fb9b009d0c5f08de63eb80d2da767212a26dd6c
enestasy <101826984+enestasy@users.noreply.github.com>
2024-10-10 20:17:25 +0300

    Update задание8.c

--- задание8.c
+++ задание8.c
@@ -1,3 +1,40 @@
+//Найти максимальную степень 2 на которую делится данное целое число. Примечание. Операторами цикла пользоваться нельзя.
+
+//Вариант 1
+#include <stdio.h>
+
+int max_divider(int number, int * degree) {
+    if (number % 2 != 0) {
+        return 1;
+    } else {
+        (*degree)++;
+        return 2 * max_divider(number / 2, degree);
+    }
+}
+
+int main() {
+    int number, degree = 0, divider;
+    
+    printf("Enter number: ");
+    scanf("%d", &number);
+    
+    if (number == 0) {
+        printf("The number cannot be zero.\n");
+        return 1;
+    }
+    
+    divider = max_divider(number, &degree);
+    
+    printf("The largest power-of-two divisor is: %d\n", divider);
+    printf("The degree of the divisor is: %d\n", degree);
+
+    return 0;
+}
+
+
+//Вариант 2 (работает не всегда)
+
+/*
 #include <stdio.h>
 #include <math.h>
 
@@ -19,3 +56,4 @@ int main() {
 
     return 0;
 }
+*/
